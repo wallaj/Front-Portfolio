4ca6ca43ef9cfbfad813883d8897b0653106a456
Marcos <marcosurzua4@gmail.com>
2022-03-19 00:43:44 -0300

    header, logoAP, rrss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { LogoAPComponent } from './logo-ap/logo-ap.component';
+import { RrssComponent } from './rrss/rrss.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    LogoAPComponent,
+    RrssComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/rrss/rrss.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rrss',
+  templateUrl: './rrss.component.html',
+  styleUrls: ['./rrss.component.css']
+})
+export class RrssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
